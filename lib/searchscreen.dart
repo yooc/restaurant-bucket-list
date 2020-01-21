034184f8bda79273fd1c18359d0e8be152f4112b
Courtney Yoo <yooc53@gmail.com>
2020-01-20 20:31:57 -0800

    Successful network call to simplified endpoint

--- lib/searchscreen.dart
+++ lib/searchscreen.dart
@@ -1,15 +1,52 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:restaurant_bucket_list/restaurant.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
+  const SearchScreen();
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  Restaurant restaurant;
+
+  @override
+  void initState() {
+    super.initState();
+    this.fetchRestaurantData();
+  }
+
+  Future<Restaurant> fetchRestaurantData() async {
+    final response = await http.get(
+        'https://developers.zomato.com/api/v2.1/restaurant?res_id=17145495',
+        headers: {'user-key': 'Replace with api key'});
+
+    setState(() {
+      var jsonData = jsonDecode(response.body);
+      restaurant = Restaurant.fromJson(jsonData);
+    });
+    return restaurant;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextField(
-        decoration: InputDecoration(
-          prefixIcon: Icon(Icons.search),
-          labelText: 'Search query',
+      child: Column(children: <Widget>[
+        TextField(
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            labelText: 'Search query',
+          ),
         ),
-      ),
+        Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: Text(restaurant.name),
+        ),
+      ]),
     );
   }
 }
